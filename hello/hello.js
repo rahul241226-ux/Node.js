0bd0d79f5f799d43b7c55990c03613d15f82ab0a
Rahul <rahul.241226@ncit.edu.np>
2026-08-15 22:21:45 +0545

    JSON package:start

--- hello/hello.js
+++ hello/hello.js
@@ -1,4 +1,4 @@
-console.log("hello its my fist node with js");
+console.log("hello its my fist node with js with rahul");
 
 
 // jSon file can create by using (npm init) then put start name or anything and write "node" and your "file name" 
