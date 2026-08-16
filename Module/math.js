ca4fef4c919a0ae5cdca172d3a4dcd63d7770311
Rahul <rahul.241226@ncit.edu.np>
2026-08-16 11:26:00 +0545

    Modules

---
+++ Module/math.js
@@ -0,0 +1,46 @@
+// Default exporting theorem
+
+
+function add(a,b){
+
+return a+b;
+
+}
+
+
+//SUBTRACTION
+function sub(a,b){
+
+return a-b;
+
+}
+// module.exports ="Rahul"
+module.exports ={
+    
+    
+    // ADD:add,
+    // SUB:sub,
+    add,
+    sub,
+
+
+}
+
+
+
+
+
+
+
+
+
+//2. using exort objects
+
+
+
+// exports.add = (a,b) => {a+b};
+
+// exports.sub = (a,b) => {a-b}
+
+
+
---
+++ Module/module.js
@@ -0,0 +1,17 @@
+// const math= require('./math')/
+// console.log(math);
+// console.log(math.SUB(5,6),);
+// console.log(math.ADD(5,6),);
+
+
+
+const {add,sub}= require('./math')//.require functon to import those function
+console.log(sub(5,6),);
+
+
+
+
+
+// const math = require("http")// this packag euse to make web servers
+// const math = require("fs")// use to file handling
+// const math = require("./fs")// it will search value in current directories
