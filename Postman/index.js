fef7fa7cc5c6cad7546c51ffab8bb3120863cc17
Rahul <rahul.241226@ncit.edu.np>
2026-08-18 09:00:45 +0545

    status code

--- Postman/index.js
+++ Postman/index.js
@@ -1,3 +1,115 @@
+//POSTMAN
+
+
+// const express = require("express");
+// const fs = require('fs')
+// const users = require("./MOCK_DATA.json");
+
+// const app = express();
+// const PORT = 8000;
+
+
+// app.use(express.urlencoded({extended:false}));
+
+// // GET API - HTML
+// app.get("/users", (req, res) => {
+//     const html = `
+//         <ul>
+//             ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+//         </ul>
+//     `;
+
+//     res.send(html);
+// });
+
+// // REST API - Get all users
+// app.get("/api/users", (req, res) => {
+//     return res.json(users);
+// });
+
+// // REST API - Get user by ID
+// app.get("/api/users/:id", (req, res) => {
+
+//     const id = Number(req.params.id);
+
+//     const user = users.find((user) => user.id === id);
+
+//     return res.json(user);
+// });
+
+
+
+
+
+// app.post('/api/users', (req, res) => {
+
+//     // create new users
+//     const body = req.body;
+//     if (!body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
+//         return res.status(400).json({msg:"all fields are req.."})
+//     }
+
+
+//     console.log("body", body);
+//     users.push({...body,id: users.length + 1});
+//     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err, data) =>{
+
+
+//     return res.status(201).json({ status: "success", id: users.length+1 });
+
+//     })
+    
+// })
+
+
+
+
+
+// app.patch('/api/users/:id', (req, res) => {
+
+//     // edit the user with id
+
+//     return res.json({ status: "pending" });
+// })
+
+
+// app.delete('/api/users/:id', (req, res) => {
+
+
+//     // delete the user with id
+
+//     return res.json({ status: "pending" });
+
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`Server started at PORT ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//STATUS ERRORS AND CODE
+
+
+
+
+
+
 const express = require("express");
 const fs = require('fs')
 const users = require("./MOCK_DATA.json");
@@ -25,14 +137,14 @@ app.get("/api/users", (req, res) => {
 });
 
 // REST API - Get user by ID
-app.get("/api/users/:id", (req, res) => {
+app.route("/api/users/:id").get, (req, res) => {
 
     const id = Number(req.params.id);
 
     const user = users.find((user) => user.id === id);
-
+if(!user) return res.status(404).json({error:'user not found'})
     return res.json(user);
-});
+};
 
 
 
@@ -42,16 +154,23 @@ app.post('/api/users', (req, res) => {
 
     // create new users
     const body = req.body;
+    if (!body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title) {
+        return res.status(400).json({msg:"all fields are req.."})
+    }
+
+    
     console.log("body", body);
     users.push({...body,id: users.length + 1});
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err, data) =>{
 
 
-    return res.json({ status: "success", id: users.length+1 });
+    return res.status(201).json({ status: "success", id: users.length+1 });
 
     })
     
 })
+
+
 
 
 
